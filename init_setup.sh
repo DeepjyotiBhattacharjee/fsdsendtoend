b9209df4656056fad303ba9f5a26dc862a4d3feb
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-01-09 22:47:00 +0530

    created shell script for env automation

---
+++ init_setup.sh
@@ -0,0 +1,16 @@
+echo [$(date)]: "START"
+
+echo [$(date)]: "creating env with python 3.11.3 version"
+
+conda create --prefix ./env python=3.11.3 -y
+
+echo [$(date)]: "Activating the environment"
+
+source activate ./env 
+
+echo [$(date)]: "Installing the dev requirements"
+
+pip install -r requirements.txt
+
+echo [$(date)]: "END" 
+
